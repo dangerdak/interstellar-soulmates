84ea489cf516ffa14294e383df46cfe3cf0bcabb
Dakshina Scott <dakshina.scott@gmail.com>
2017-08-16 18:56:30 +0100

    Adds sign in query relates #7

---
+++ src/model/signIn.js
@@ -0,0 +1,18 @@
+const dbConnection = require('./db/dbConnection');
+
+const signIn = (email, cb) => {
+  const sqlGetPassword = `SELECT password FROM users WHERE email = $1;`;
+  dbConnection.query(sqlGetPassword, [email], (err, res) => {
+    if (err) {
+      cb(err);
+    } else if (!res.rows[0]) {
+      // TODO could create a custom error
+      cb(null, '');
+    } else {
+      cb(null, res.rows[0].password);
+    }
+  });
+};
+
+signIn('x@g', console.log);
+module.exports = signIn;
