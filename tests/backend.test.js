ea4e5fbc70795048d7b0a8e56b4bbf2a463d86ed
Constantina <polyccon@gmail.com>
2017-08-16 20:02:57 +0100

    wording amended after Daks comment

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -39,17 +39,17 @@ test('Should be able to get a user by their id', t => {
   });
 });
 
-test('Should add a new user', t => {
+test('Should add a new profile', t => {
   const newuser = {id: 5, user_id: 5, name: 'Thor', species: null, image_url: null, cns: true, legs: '5', interests: 'interstellar ping-pong' };
   request(app)
-    .post(`/user/edit`)
+    .post(`/editprofile`)
     .send(newuser)
     .expect(201)
     .expect('Content-Type', /json/)
     .end((err, res) => {
       t.same(res.statusCode, 201, 'Status code is 201');
       t.error(err, 'No error');
-      t.same(res.body[0].name, 'Thor', 'Should add new user to profiles');
+      t.same(res.body[0].name, 'Thor', 'Should add new user profile to profiles table');
       t.end();
     });
 });
