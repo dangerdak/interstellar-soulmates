53a70721a7879584c4d039e1206e1276386cc68d
Bart Bucknill <bartbucknill@hotmail.com>
2017-08-16 20:02:07 +0100

    add user_id where condition to updateProfile

--- src/model/updateProfile.js
+++ src/model/updateProfile.js
@@ -1,8 +1,8 @@
 const dbConnection = require('./db/dbConnection');
 
 const updateProfile = (userData, userId, cb) => {
-  const sqlUpdate = 'UPDATE profiles SET (name, species, image_url, cns, legs, interests) = ($1, $2, $3, $4, $5, $6);';
-  dbConnection.query(sqlUpdate, [userData.name, userData.species, userData.image_url, userData.cns, userData.legs, userData.interests], (err, res) => {
+  const sqlUpdate = 'UPDATE profiles SET (name, species, image_url, cns, legs, interests) = ($1, $2, $3, $4, $5, $6) WHERE user_id=$7;';
+  dbConnection.query(sqlUpdate, [userData.name, userData.species, userData.image_url, userData.cns, userData.legs, userData.interests, userId], (err, res) => {
     if (err) {
       cb(err);
     } else {
