e49cc29188871d5e8f9218a0295cac5b40834d05
Constantina <polyccon@gmail.com>
2017-08-17 15:58:32 +0100

    installed express session module
    relates to #25

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -6,7 +6,7 @@ const signUp = require('./signUp');
 const editProfile = require('./editProfile');
 const singles = require('./singles');
 const errors = require('./errorhandlers');
-
+const session = require('express-session');
 
 router.get('/', home);
 router.get('/singles', singles);
@@ -16,6 +16,7 @@ router.get('/edit-profile', editProfile);
 router.post('/sign-up', function(req, res){
   res.redirect('/edit-profile');
 });
+router.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: true, cookie: { maxAge: 60000 }}));
 router.use(errors.client);
 router.use(errors.server);
 
