d50b8fb39a1a2587fe23468abfac5700da9b4f86
Constantina <polyccon@gmail.com>
2017-08-17 10:35:28 +0100

    signup controller working

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const home = require('./home');
+const signUp = require('./signUp');
 
 router.get('/', home);
+router.get('/sign-up', signUp);
 
 module.exports = router;
